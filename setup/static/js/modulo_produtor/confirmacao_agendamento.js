9f41b4ef6d230f14c868bae133d5fce5f63300dc
Allan Ribeiro <akosr91@gmail.com>
2024-04-17 17:45:10 -0300

    ajuste na confirmação do agendamento

--- setup/static/js/modulo_produtor/confirmacao_agendamento.js
+++ setup/static/js/modulo_produtor/confirmacao_agendamento.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
+    const navbar = document.querySelector('#navbar')
+    navbar.style.display = 'none';
 
     const btnSair = document.querySelector('#btnSair')
     btnSair.addEventListener('click', function(){
