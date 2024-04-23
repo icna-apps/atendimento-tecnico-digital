b25c6495d8d08832745afd8fc90a74c07347f395
Allan Ribeiro <akosr91@gmail.com>
2024-04-23 10:46:55 -0300

    avanços em meus dados

--- setup/static/js/modulo_produtor/meus_dados.js
+++ setup/static/js/modulo_produtor/meus_dados.js
@@ -3,26 +3,97 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#formCelular').mask('(00) 00000-0000');
     $('#formDataNascimento').mask('00/00/0000');
 
-
+    const formDataNascimento = document.querySelector('#formDataNascimento')
     formDataNascimento.addEventListener('change', function() {
-        // A propriedade correta para obter o valor de um campo de input é `value`, não `ariaValue`
         let dataInserida = formDataNascimento.value;
-    
-        // Verificar se a data é válida. A função `Date.parse()` retorna NaN se a data for inválida
         let dataConvertida = Date.parse(dataInserida);
-    
-        // NaN é "falsy", então !dataConvertida será verdadeiro se dataConvertida for NaN
         if (!dataConvertida) {
-            // Usando sweetAlert para mostrar uma mensagem de erro
             sweetAlert(
                 tittle='Data de nascimento inválida!',
                 icon='warning',
                 iconColor='red',
             )
-            // Resetar o valor do campo de data para vazio
             formDataNascimento.value = '';
         }
     });
+
+
+    const btnSalvar = document.querySelector('#btnSalvar')
+    btnSalvar.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('teste')
+    })
+
+    function salvarDadosPessoais(){
+        //Verificar preenchimento dos campos
+        let preenchimento_incorreto = verificarCampos()
+        if (preenchimento_incorreto === false) {
+            return;
+        }
+
+        //Enviar para o backend
+            //definir o caminho
+            postURL = '/produtor/realizar-agendamento/'
+    
+            //pegar os dados
+            let formData = new FormData(document.getElementById('formNovoAtendimento'));
+    
+            //enviar 
+            fetch(postURL, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-CSRFToken': getCSRFToken(),
+                }
+            })
+        
+        //Retorno do Servidor
+        .then(response => {
+            if (!response.ok) {
+                sweetAlert('<span style="font-weight:normal">Erro! Agendamento <b style="color:red">não realizado!</b></span>', 'error', 'red');
+                throw new Error('Server response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.agendado === "sim") {
+                const idAgendamento = data.id_agendamento;
+                window.location.href = `/produtor/confirmacao-atendimento/${idAgendamento}/`;
+            } else {
+                sweetAlert('<span style="font-weight:normal">Erro! Agendamento <b style="color:red">não realizado!</b></span>', 'error', 'red');
+            }
+        })
+        .catch(error => {
+            console.error('Fetch operation error:', error);
+        });
+    }
+
+
+    function verificarCampos() {
+        const campos = [
+            { id: 'formDataNascimento', mensagem: 'Informe a <b>Atividade Produtiva</b>!' },
+            { id: 'formCelular', mensagem: 'Informe o <b>Tópico do Atendimento</b>!' },
+            { id: 'id_data', mensagem: 'Informe a <b>Data</b>!' },
+            { id: 'id_hora', mensagem: 'Informe a <b>Hora</b>!' },
+        ];
+    
+        let mensagensErro = campos.reduce((mensagens, campo) => {
+            const elemento = document.getElementById(campo.id);
+            if (!elemento || elemento.value === '') {
+                mensagens.push(campo.mensagem);
+            }
+            return mensagens;
+        }, []);
+    
+        if (mensagensErro.length > 0) {
+            const mensagem = mensagensErro.join('<br>')
+            sweetAlertPreenchimento({ mensagem })
+            return false;
+        }
+    
+        return true;
+    }
 
 
 });
